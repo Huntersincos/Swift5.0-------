51746996559dc956130c5d56ca4d8c1822c5a5dd
wenze <wenze@sinovatech.com>
2020-09-25 18:57:35 +0800

    swift sdwebiamge test finsh 30% imageview

---
+++ VedioPlay/VedioPlay/Vedio/LoadURLImageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoadURLImageViewController.swift
+//  VedioPlay
+//
+//  Created by wenze on 2020/9/23.
+//  Copyright © 2020 wenze. All rights reserved.
+//
+
+import UIKit
+
+class LoadURLImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ VedioPlay/VedioPlay/Vedio/OtherClassViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OtherClassViewController.swift
+//  VedioPlay
+//
+//  Created by wenze on 2020/9/25.
+//  Copyright © 2020 wenze. All rights reserved.
+//
+
+import UIKit
+
+class OtherClassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
